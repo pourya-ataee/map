8229c8f7982b12fe7a5877f26a53c0c9c84ded73
Pourya Ataee <p.ataee.dev@gmail.com>
2025-02-15 17:18:43 +0330

    build(config): add production env

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,11 +4,11 @@ import { ISearchAddressResponse } from "@/models/ISearchAddressResponse";
 import { generateRandomAddress, generateRandomLatLng } from "@/utils";
 
 export const handlers = [
-	http.get("http://localhost:3000/api/search/get-address", () => {
+	http.get(`${process.env.NEXT_PUBLIC_BASE_URL}search/get-address`, () => {
 		return HttpResponse.json<IGetAddressResponse>(generateRandomAddress());
 	}),
 
-	http.get("http://localhost:3000/api/search/search-address", () => {
+	http.get(`${process.env.NEXT_PUBLIC_BASE_URL}search/search-address`, () => {
 		return HttpResponse.json<ISearchAddressResponse>(generateRandomLatLng());
 	}),
 ];
